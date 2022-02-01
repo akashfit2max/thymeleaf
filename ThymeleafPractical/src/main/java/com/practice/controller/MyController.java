84762ece2d1c2cc4d7e21be777dd05c1c93f3941
akash <akash@LAPTOP-8F3BRJOB>
2022-02-01 21:24:00 +0530

    learning thymeleaf

---
+++ ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -0,0 +1,21 @@
+package com.practice.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about(Model model) {
+		System.out.println("inside about handler");
+		model.addAttribute("name", "Akash");
+		model.addAttribute("currentDate", new Date());
+		return "about";
+	}
+
+}
