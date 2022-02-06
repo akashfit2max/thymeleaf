4a2c397f866dc7ec006b62c4b2646b91c4c54e8a
akash <akash@LAPTOP-8F3BRJOB>
2022-02-06 22:33:08 +0530

    conditional statement

--- ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
+++ ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.practice.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,9 +15,32 @@ public class MyController {
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String about(Model model) {
 		System.out.println("inside about handler");
-		model.addAttribute("name", "Akash");
+		model.addAttribute("name", "Ashish kumar sahoo");
 		model.addAttribute("currentDate", new Date());
 		return "about";
+	}
+
+	@GetMapping("/loop")
+	public String loopHandler(Model model) {
+		List<String> names = List.of("Ankit", "Laxmi", "John", "Ashish");
+		model.addAttribute("names", names);
+		return "loop";
+	}
+
+//	conditional operator
+	@GetMapping("/conditional")
+	public String condHandler(Model model) {
+		System.out.println("inside cond op page");
+		model.addAttribute("isActive", true);
+		return "cpage";
+	}
+	
+//	if unless
+	
+	@GetMapping("/isunless")
+	public String isunless(Model model) {
+		model.addAttribute("gender", "F");
+		return "cpage";
 	}
 
 }
